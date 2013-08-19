26dd11c199d56219a9eca6393827eee917de2ebb
Alistair Dobke <adobke@gmail.com>
2013-08-19 14:50:36 -0700

    working couch teleop

--- src/couch_teleop/src/couch_teleop.cpp
+++ src/couch_teleop/src/couch_teleop.cpp
@@ -3,6 +3,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include "couch_control/MotorCommand.h"
+#include <algorithm>
 // %EndTag(INCLUDE)%
 // %Tag(CLASSDEF)%
 class CouchTeleop
@@ -15,22 +16,17 @@ private:
   
   ros::NodeHandle nh_;
 
-  int linear_, angular_;
-  double l_scale_, a_scale_;
+  double x_scale_, y_scale_;
   ros::Publisher cmd_pub_;
   ros::Subscriber joy_sub_;
   
 };
 // %EndTag(CLASSDEF)%
 // %Tag(PARAMS)%
-CouchTeleop::CouchTeleop():
-  linear_(1),
-  angular_(2)
+CouchTeleop::CouchTeleop()
 {
-  nh_.param("axis_linear", linear_, linear_);
-  nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
+  nh_.param("x_scale", x_scale_, 1.0);
+  nh_.param("y_scale", y_scale_, 1.0);
 // %EndTag(PARAMS)%
 // %Tag(PUB)%
   cmd_pub_ = nh_.advertise<couch_control::MotorCommand>("/test", 1);
@@ -43,8 +39,14 @@ CouchTeleop::CouchTeleop():
 void CouchTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
   couch_control::MotorCommand cmd;
-  cmd.left = 300 * a_scale_*joy->axes[angular_];
-  cmd.right = 300 * l_scale_*joy->axes[linear_];
+  double xaxis = joy->axes[0] * x_scale_;
+  double yaxis = joy->axes[1] * y_scale_;
+  cmd.left = (yaxis - xaxis) * 300.0;
+  cmd.right = (yaxis + xaxis) * 300.0;
+  cmd.left = std::max<double>(std::min<double>(cmd.left,300.0),-300.0);
+  cmd.right = std::max<double>(std::min<double>(cmd.right,300.0),-300.0);
+  //cmd.left = 300.0 * a_scale_*joy->axes[angular_];
+  //cmd.right = 300.0 * l_scale_*joy->axes[linear_];
   cmd_pub_.publish(cmd);
 }
 // %EndTag(CALLBACK)%
